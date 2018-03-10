8461d71f4a554b4ab0128f03b6f7d7c464394705
Alex Yang <a.yang2@share.epsb.ca>
2018-03-10 19:47:23 +0000

    Declared and initiated question answers

--- ArithmeticAssignment.java
+++ ArithmeticAssignment.java
@@ -4,6 +4,8 @@ public class ArithmeticAssignment
 {
 	public static void main(String args[])
 	{
-		
+		int p1 = 79 + 3 *(4 + 82 - 68) - 7 + 19; //145
+		int p2 = (179 + 21 + 10) / 7 + 181; //211
+		int p3 = 10389 * 56 * 11 + 2246; //6401870  
 	}
 }
