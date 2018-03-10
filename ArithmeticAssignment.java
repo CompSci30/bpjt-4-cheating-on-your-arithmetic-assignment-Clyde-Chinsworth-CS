fcd51952eeec13792ee0e49e59267dc6bf92bd2b
Alex Yang <a.yang2@share.epsb.ca>
2018-03-10 19:50:52 +0000

    Printed third question

--- ArithmeticAssignment.java
+++ ArithmeticAssignment.java
@@ -10,6 +10,6 @@ public class ArithmeticAssignment
 		
 		println("79 + 3 * (4 + 82 - 68) -7 + 19 = " + p1);
 		println("(179 + 21 + 10) / 7 + 181 = " + p2);
-		   
+		println("10389 * 56 * 11 + 2246 = " + p3);    
 	}
 }
